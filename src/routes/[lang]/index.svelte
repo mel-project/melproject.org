4ce0c7942cd89207502633eaec6186e14e402d48
Guy Fierri <shmendez@Guys-MBP.lan>
2022-06-06 17:10:42 -0500

    finished pug move

--- src/routes/[lang]/index.svelte
+++ src/routes/[lang]/index.svelte
@@ -7,244 +7,131 @@
   $: l10n = l10nLoad({ ...base, ...layer }, $lang);
 
   let carouselPosn = 0;
+  let carouselimgs = "homepage-participate.png homepage-build.png homepage-learn.png".split(" ")
+  $: carouselimg = carouselimgs[carouselPosn]
+
 </script>
 
-<MainLayout title={l10n("nav/home")}>
-  <!-- main hero -->
-  <div class="section section--hero">
-    <div class="section__inner container">
-      <div class="row">
-        <div class="col">
-          <div class="alert alert-cta">
-            <div class="ml-3">
-              {@html l10n("wallet-cta")}
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-12 col-lg-9">
-          <h1>{@html l10n("tagline")}</h1>
-          <p class="text--blead">{@html l10n("subtitle")}</p>
-        </div>
-        <div class="col-6 col-md-12">
-          <a href="#community-sect" class="btn btn-outline-primary btn-lg">
-            {l10n("join-the-community")}
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+MainLayout(title!='{l10n("nav/home")}')
+      .section.section--hero
+        .section__inner.container
+          .row
+            .col
+              .alert.alert-cta
+                .ml-3
+                  | {@html l10n("wallet-cta")}
+          .row
+            .col-12.col-lg-9
+              h1 {@html l10n("tagline")}
+              p.text--blead {@html l10n("subtitle")}
+            .col-6.col-md-12
+              a.btn.btn-outline-primary.btn-lg(href='#community-sect')
+                | {l10n("join-the-community")}
+      // why themelio
+      .section.section--darken
+        .section__inner.container
+          .row
+            .col
+              h2 {l10n("why-title")}
+          .row
+            .rethink-point.col-lg
+              img.rethink-icon(src='/images/smillust/keyhole.png', alt='circle with key')
+              div {@html l10n("why-1")}
+            .rethink-point.col-lg
+              img.rethink-icon(src='/images/smillust/fork.png', alt='circle with fork')
+              div {@html l10n("why-2")}
+          .row
+            .rethink-point.col-lg
+              img.rethink-icon(src='/images/smillust/mel-small.png', alt='circle with mel')
+              div {@html l10n("why-3")}
+            .rethink-point.col-lg
+              img.rethink-icon(src='/images/smillust/scalability.png', alt='scalable')
+              div {@html l10n("why-4")}
+      // melmint
+      .section
+        .section__inner.container
+          .row
+            .col-12.col-lg-6
+              img.img-fluid(src='/images/smillust/mel-big.png', style='mix-blend-mode: darken;', alt='a mel floating on an ocean')
+            .col-12.col-lg-6
+              h2 {@html l10n("melmint-title")}
+              div {@html l10n("melmint-summary")}
+      // radiobuttons
+      .section.section--darken
+        .section__inner.container
+          .row
+            .col-12.pb-3(class!='{carouselPosn}')
+              h2 {@html l10n("explore-themelio")}
+          .row
+            .col-md
+              .tab-set
+                .tab-button(class:tab-button-active!="{carouselPosn == 0}" on:click!='{() => (carouselPosn = 0)}')
+                  | {l10n("participate")}
+                .tab-button(class:tab-button-active!="{carouselPosn == 1}" on:click!='{() => (carouselPosn = 1)}')
+                  | {l10n("build")}
+                .tab-button(class:tab-button-active!="{carouselPosn == 2}" on:click!='{() => (carouselPosn = 2)}')
+                  | {l10n("learn")}
 
-  <!--why themelio-->
-  <div class="section section--darken">
-    <div class="section__inner container">
-      <div class="row">
-        <div class="col">
-          <h2>{l10n("why-title")}</h2>
-        </div>
-      </div>
-      <div class="row">
-        <div class="rethink-point col-lg">
-          <img
-            class="rethink-icon"
-            src="/images/smillust/keyhole.png"
-            alt="circle with key"
-          />
-          <div>{@html l10n("why-1")}</div>
-        </div>
-        <div class="rethink-point col-lg">
-          <img
-            class="rethink-icon"
-            src="/images/smillust/fork.png"
-            alt="circle with fork"
-          />
-          <div>{@html l10n("why-2")}</div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="rethink-point col-lg">
-          <img
-            class="rethink-icon"
-            src="/images/smillust/mel-small.png"
-            alt="circle with mel"
-          />
-          <div>{@html l10n("why-3")}</div>
-        </div>
-        <div class="rethink-point col-lg">
-          <img
-            class="rethink-icon"
-            src="/images/smillust/scalability.png"
-            alt="scalable"
-          />
-          <div>{@html l10n("why-4")}</div>
-        </div>
-      </div>
-    </div>
-  </div>
+              +if('carouselPosn == 0')
+                div {@html l10n("participate-desc")}
+                +elseif('carouselPosn == 1')
+                  div {@html l10n("build-desc")}
+                +else
+                  div {@html l10n("learn-desc")}
 
-  <!--melmint-->
-  <div class="section">
-    <div class="section__inner container">
-      <div class="row">
-        <div class="col-12 col-lg-6">
-          <img
-            class="img-fluid"
-            src="/images/smillust/mel-big.png"
-            style="mix-blend-mode: darken;"
-            alt="a mel floating on an ocean"
-          />
-        </div>
-        <div class="col-12 col-lg-6">
-          <h2>{@html l10n("melmint-title")}</h2>
-          {@html l10n("melmint-summary")}
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <!--radiobuttons-->
-  <div class="section section--darken">
-    <div class="section__inner container">
-      <div class="row">
-        <div class="col-12 pb-3">
-          <h2>{@html l10n("explore-themelio")}</h2>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md">
-          <div class="tab-set">
-            <div
-              class="tab-button"
-              class:tab-button-active={carouselPosn == 0}
-              on:click={() => (carouselPosn = 0)}
-            >
-              {l10n("participate")}
-            </div>
-            <div
-              class="tab-button"
-              class:tab-button-active={carouselPosn == 1}
-              on:click={() => (carouselPosn = 1)}
-            >
-              {l10n("build")}
-            </div>
-            <div
-              class="tab-button"
-              class:tab-button-active={carouselPosn == 2}
-              on:click={() => (carouselPosn = 2)}
-            >
-              {l10n("learn")}
-            </div>
-          </div>
-
-          {#if carouselPosn == 0}
-            <div>{@html l10n("participate-desc")}</div>
-          {:else if carouselPosn == 1}
-            <div>{@html l10n("build-desc")}</div>
-          {:else}
-            <div>{@html l10n("learn-desc")}</div>
-          {/if}
-        </div>
-        <div class="col-md">
-          <img
-            class="img-fluid"
-            src={carouselPosn == 0
-              ? "/images/homepage-participate.png"
-              : carouselPosn == 1
-              ? "/images/homepage-build.png"
-              : "/images/homepage-learn.png"}
-            style="mix-blend-mode: darken;"
-            alt="carousel"
-          />
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <!--community-->
-  <div class="section" id="community-sect">
-    <div class="section__inner container">
-      <div class="row">
-        <div class="col-lg-2" />
-        <div class="col text-center">{@html l10n("community")}</div>
-        <div class="col-lg-2" />
-      </div>
-      <div class="row">
-        <div class="col-md">
-          <a class="community-card" href="https://discord.gg/qfg35paESn">
-            <div class="community-icon">
-              <img src="/images/social_media/discord.png" alt="discord" />
-            </div>
-            <div class="community-desc">{@html l10n("community/discord")}</div>
-          </a>
-        </div>
-        <div class="col-md">
-          <a class="community-card" href="https://t.me/themeliolabs">
-            <div class="community-icon">
-              <img src="/images/social_media/telegram.png" alt="telegram" />
-            </div>
-            <div class="community-desc">{@html l10n("community/telegram")}</div>
-          </a>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md">
-          <a class="community-card" href="https://twitter.com/themeliolabs">
-            <div class="community-icon">
-              <img src="/images/social_media/twitter.png" alt="twitter" />
-            </div>
-            <div class="community-desc">{@html l10n("community/twitter")}</div>
-          </a>
-        </div>
-        <div class="col-md">
-          <a class="community-card" href="https://github.com/themeliolabs/">
-            <div class="community-icon">
-              <img src="/images/social_media/github.png" alt="github" />
-            </div>
-            <div class="community-desc">{@html l10n("community/github")}</div>
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <!--blog-->
-
-  <div class="section section section--darken" id="blog-sect">
-    <div class="section__inner container">
-      <div class="row">
-        <div class="col">
-          <h2>{@html l10n("blog")}</h2>
-          <div class="rss-container">
-            <rssapp-wall id="XBZoy5LUiQA6UTZM" />
-          </div>
-          <script
-            src="https://widget.rss.app/v1/wall.js"
-            type="text/javascript"
-            async></script>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <!--team-->
-
-  <div class="section" id="team-sect">
-    <div class="section__inner container">
-      <div class="row">
-        <div class="col-lg" />
-        <div class="col-lg-8 text-center">
-          <h2>{@html l10n("team")}</h2>
-          <a href="team.html" class="btn btn-outline-primary btn-lg">
-            {@html l10n("view-our-team")}
-          </a>
-        </div>
-        <div class="col-lg" />
-      </div>
-    </div>
-  </div>
-</MainLayout>
-
+            .col-md
+              // community
+              img.img-fluid(src!="/images/{carouselimg}" style="mix-blend-mode: darken;" alt="carousel")
+      
+      #community-sect.section
+        .section__inner.container
+          .row
+            .col-lg-2
+              .col.text-center {@html l10n("community")}
+              .col-lg-2
+              .row
+                .col-md
+                  a.community-card(href='https://discord.gg/qfg35paESn')
+                    .community-icon
+                      img(src='/images/social_media/discord.png', alt='discord')
+                    .community-desc {@html l10n("community/discord")}
+                .col-md
+                  a.community-card(href='https://t.me/themeliolabs')
+                    .community-icon
+                      img(src='/images/social_media/telegram.png', alt='telegram')
+                    .community-desc {@html l10n("community/telegram")}
+              .row
+                .col-md
+                  a.community-card(href='https://twitter.com/themeliolabs')
+                    .community-icon
+                      img(src='/images/social_media/twitter.png', alt='twitter')
+                    .community-desc {@html l10n("community/twitter")}
+                .col-md
+                  a.community-card(href='https://github.com/themeliolabs/')
+                    .community-icon
+                      img(src='/images/social_media/github.png', alt='github')
+                    .community-desc {@html l10n("community/github")}
+          // blog
+        #blog-sect.section.section.section--darken
+          .section__inner.container
+            .row
+              .col
+                h2 {@html l10n("blog")}
+                .rss-container
+                  rssapp-wall#XBZoy5LUiQA6UTZM
+                script(src='https://widget.rss.app/v1/wall.js', type='text/javascript', async='')
+        // team
+        #team-sect.section
+          .section__inner.container
+            .row
+              .col-lg
+                .col-lg-8.text-center
+                  h2 {@html l10n("team")}
+                  a.btn.btn-outline-primary.btn-lg(href='team.html')
+                    | {@html l10n("view-our-team")}
+                .col-lg
+</template>
 <style lang="scss">
   .tab-set {
     display: flex;
