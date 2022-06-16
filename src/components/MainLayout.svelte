592d400afd1e7a681247f582f0947d2de8a417b6
Shane Mendez <shane@themelio.org>
2022-06-16 17:13:20 -0500

    idk

--- src/components/MainLayout.svelte
+++ src/components/MainLayout.svelte
@@ -26,7 +26,7 @@
     link(rel='stylesheet' href='https://cdn.jsdelivr.net/gh/chiron-fonts/chiron-sans-hk-pro@1.007/build/webfont/css/vf-italic.css')
     link(href='https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300;400;500;700&display=swap' rel='stylesheet')
     link(href='https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700' rel='stylesheet')
-  svelte:body({$lang}) 
+  svelte:body({$lang})
   svelte:window(bind:innerWidth)
   +if('debugging == true')
     div(style="position: sticky; top: 0;") 
