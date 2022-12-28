732b15dd2d2df55ae93f9c88148da1d1a0a26331
nullchinchilla <nullchinchilla@pm.me>
2022-12-28 17:50:05 -0600

    as

--- src/routes/[lang]/+page.svelte
+++ src/routes/[lang]/+page.svelte
@@ -7,7 +7,7 @@
 
 	import base from "@l10n/base.yaml";
 	import layer from "@l10n/index.l10n.yaml";
-	$: l10n = l10nLoad({ ...base, ...layer }, $lang);
+	$: l10n = l10nLoad({ ...base, ...layer }, $lang); 
 
 	let carouselPosn = 0;
 	let carouselimgs =
--- svelte.config.js
+++ svelte.config.js
@@ -1,30 +1,28 @@
-import adapter from '@sveltejs/adapter-netlify';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-netlify";
+import preprocess from "svelte-preprocess";
 // import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter({
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions
-			edge: true,
+  kit: {
+    adapter: adapter({
+      // if true, will create a Netlify Edge Function rather
+      // than using standard Node-based functions
+      edge: true,
 
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app.
-			// if `edge` is true, this option cannot be used
-			split: false
-		}),
-		alias: {
-			'@l10n/*': 'src/l10n/*',
-		}
-
-	},
-
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app.
+      // if `edge` is true, this option cannot be used
+      split: false,
+    }),
+    alias: {
+      "@l10n/*": "src/l10n/*",
+    },
+  },
 };
 
 export default config;
