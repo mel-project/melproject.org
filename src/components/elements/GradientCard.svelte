8bda81361410791ebbc97f3b82d8a9eb44fc161a
Shane Mendez <shane@themelio.org>
2022-12-30 15:02:30 -0600

    cards that float

--- src/components/elements/GradientCard.svelte
+++ src/components/elements/GradientCard.svelte
@@ -1,25 +1,34 @@
 <script lang="ts">
+    let _class = "";
+    export { _class as class };
 </script>
 
 <template>
-    <div class="card">
-        <slot></slot>
+    <div class={_class + " card"}>
+        <slot />
     </div>
 </template>
 
 <style lang="scss">
-    @use '../../stylesheets/variables.scss' as colors;
+    @use "../../stylesheets/variables.scss" as colors;
     .card {
         border: none;
         width: 100%;
         background: radial-gradient(
-                325.46% 235.35% at 95.25% 89.76%,
-            lighten(colors.$green, 10%) 0%,
-                rgba(137, 90, 191, .1) 5.53%,
+                400% 400% at 110% 100%,
+                lighten(colors.$teal, 20%) 8%,
+                rgba(137, 90, 191, 0.1) 25%,
+                white 40%
             )
             /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */;
         mix-blend-mode: normal;
         border-radius: 20px;
+        padding: 3rem 2rem;
+
+        transition: 400ms all;
+        &:hover {
+            // box-shadow: 10px 10px 5px rgba(0,0,0,.1);
+            transform: translate(0px, -5px);
+        }
     }
 </style>
-
--- src/components/page-sections/HomeContent.svelte
+++ src/components/page-sections/HomeContent.svelte
@@ -5,20 +5,20 @@
 <template>
     <div class="content-frame">
         <div class="content">
-            <GradientCard>
-                <h1>title</h1>
+            <GradientCard class="centered">
+                <h2>Privacy first.<br> Privacy Second.</h2>
+                <p>Our unique encrypted architectures ensures that Aztec transactions are always kept from prying eyes, while also retaining auditably and compliance</p>
+            </GradientCard>
+            <GradientCard class="centered">
+                <h2>Defi. DAOS. NFTs.<br> The wat it should be</h2>
+                <p>Aztex connect allows users to confidently access world class DeFi services on Ethereum up to 100x cost savings</p>
+            </GradientCard>
+            <GradientCard class="centered">
+                <h2>title</h2>
                 <p>content</p>
             </GradientCard>
-            <GradientCard>
-                <h1>title</h1>
-                <p>content</p>
-            </GradientCard>
-            <GradientCard>
-                <h1>title</h1>
-                <p>content</p>
-            </GradientCard>
-            <GradientCard>
-                <h1>title</h1>
+            <GradientCard class="centered">
+                <h2>title</h2>
                 <p>content</p>
             </GradientCard>
         </div>
@@ -32,14 +32,19 @@
         padding: 0 clamp(0px, 10%, 100rem);
         width: 100%;
 
-        & > * {
+        & > .content {
             display: grid;
             gap: clamp(0.5rem, 2%, 2rem);
 
             grid-template-columns: 1fr 1fr;
             grid-template-rows: 1fr 1fr;
-            width: clamp(30rem, 100%, 60rem);
+            width: clamp(30rem, 100%, 75rem);
             height: min(60rem, 1000rem);
+            & > :global(.card > h2)  {
+                padding-bottom: 1rem !important;
+            }
         }
+        
     }
+
 </style>
