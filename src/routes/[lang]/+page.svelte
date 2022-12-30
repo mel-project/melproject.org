59d6e3006319f2338f82ba59bc69bd467aa20f98
Shane Mendez <shane@themelio.org>
2022-12-30 15:14:21 -0600

    hero image

--- src/routes/[lang]/+page.svelte
+++ src/routes/[lang]/+page.svelte
@@ -1,14 +1,24 @@
 <script>
-
 	import { localize } from "@l10n";
-    import HomeContent from "@src/components/page-sections/HomeContent.svelte";
+	import HomeContent from "@src/components/page-sections/HomeContent.svelte";
 	import Navbar from "@src/components/page-sections/Navbar.svelte";
-
-	
+	import HeroImage from "@src/assets/images/themelio-hero.png";
 </script>
 
 <template>
-	<Navbar />
-	<HomeContent></HomeContent>
-	{$localize("test")}
+	<div class="main-body">
+		<Navbar />
+		<img src={HeroImage} alt="Hero Image" />
+		<HomeContent />
+		{$localize("test")}
+	</div>
 </template>
+
+<style>
+	.main-body {
+		width: 100vw;
+	}
+	img{
+		max-width: 100%;
+	}
+</style>
