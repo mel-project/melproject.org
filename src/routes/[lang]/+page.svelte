acc34839a1965b6ce60d8a6d884db0bc00e3333e
nullchinchilla <nullchinchilla@pm.me>
2023-02-09 08:37:17 -0600

    first attempt at simple typography

--- src/routes/[lang]/+page.svelte
+++ src/routes/[lang]/+page.svelte
@@ -14,99 +14,168 @@
   import InfrastructureImage from "@src/assets/images/infrastructure.png";
 </script>
 
-<div class="top">
-  <img class="hero" src={HeroImage} alt="hero" />
+<div class="hero">
+  <h1>A blockchain to<br />free us from blockchains.</h1>
+  <div class="hero-text">Mel is building a clean-slate L1 for a</div>
 </div>
-<div class="container main-content">
+
+<div class="main-content container">
   <div class="row talking-point">
-    <div class="col">
-      <Card>
-        <div class="card-inner">
-          <h2>Decentralized protocols beyond blockchains.</h2>
-          <p>
-            Mel empowers a vast world of non-blockchain apps with key Web3
-            superpowers &mdash; decentralization, censorship resistance, and
-            user-centric incentives.
-            <a href="#">Our off-chain composable vision</a>
-          </p>
-          <img />
-        </div>
-      </Card>
+    <div class="col text-end">
+      <h2>Decentralization&nbsp;<br /> <b>beyond blockchains</b>.</h2>
     </div>
-    <div class="col">
-      <Card>
-        <div class="card-inner">
-          <h2>Neutral, composable, and works for everything.</h2>
-          <p>
-            Decentralized trust should be a no-brainer. Themelio is a
-            clean-slate, governance-free, and radically embeddable L1 that makes
-            accessing Web3 superpowers off-chain a breeze.
-            <a href="#">Build your first off-chain dapp</a>
-          </p>
-          <img />
-        </div>
-      </Card>
+    <div class="col desc">
+      <p>
+        Today, Web3's decentralization revolution is largely limited to
+        blockchain smart contracts. Real apps running on your device &mdash;
+        even Web3 frontends &mdash; still suffer under Web2's regime of
+        centralization and insecurity.
+      </p>
+      <p>
+        Mel presents a new paradigm. Here, "Web3 protocol" and "dApp" aren't
+        fancy names for smart contracts loosely taped onto a Web2 frontend.
+        Instead, they're next-generation network protocols and apps transformed
+        by a blockchain providing radical decentralization, censorship
+        resistance, and incentive-based security.
+      </p>
+      <a href="#">Our off-chain composable vision</a>
+    </div>
+  </div>
+
+  <div class="row talking-point">
+    <div class="col text-end">
+      <h2>Universal, composable, and <b>fiercely neutral</b>.</h2>
+    </div>
+    <div class="col desc">
+      <p>
+        Conventional blockchains are complex machines running an ecosystem of
+        on-chain smart contracts. Complexity leads to messy governance, unwieldy
+        off-chain clients, and cripple growth beyond "crypto" apps like DeFi.
+      </p>
+      <p>
+        Mel's clean-slate L1 turns this model upside-down. Everything is
+        designed for easy and secure access by regular, non-blockchain programs.
+        Trustless clients easily power everything from naming systems to data
+        storage networks.
+      </p>
+      <p>
+        And rather than adding complexity, we end up with an elegant, minimal
+        blockchain that goes back to the very root of blockchains'
+        incentive-based trust: an immutable, "code-is-law" system that liberates
+        us from contentious human governance.
+      </p>
+      <a href="#">How neutrality synergizes with composability</a>
+    </div>
+  </div>
+
+  <div class="row talking-point">
+    <div class="col text-end">
+      <h2>The first <b>zero-trust, low-volatility</b> currency.</h2>
+    </div>
+    <div class="col desc">
+      <p>
+        The future we are building needs a new kind of money. Neither volatile
+        cryptocurrencies nor fiat-pegged, oracle-trusting stablecoins point to
+        our monetary future.
+      </p>
+      <p>
+        MEL is the first ever cryptocurrency that's both stable and trustless.
+        Protocol-internal mechanisms dampen its value against computation time
+        without any oracles. A truly decentralized unit of account changes
+        everything from payments to incentive design.
+      </p>
+      <a href="#">How Melmint stabilizes MEL</a>
     </div>
   </div>
   <div class="row talking-point">
-    <div class="col">
-      <Card>
-        <div class="card-inner">
-          <h2>Monetary infrastructure for a better future.</h2>
-          <p>
-            The future we are building needs a new kind of money &mdash; $MEL is
-            the the first truly decentralized unit of account, changing
-            everything from payments to incentive design.
-          </p>
-          <img />
-        </div>
-      </Card>
+    <div class="col text-end">
+      <h2>Off-chain composability means <b>cross-chain</b> composability.</h2>
     </div>
-    <div class="col">
-      <Card>
-        <div class="card-inner">
-          <h2>From off-chain to cross-chain.</h2>
-          <p>
-            Mel’s new paradigm enabling off-chain composability also makes
-            cross-chain a breeze. And every Mel-based asset can be accessed on
-            EVM-compatible chains through trustless two-way relay contracts.
-          </p>
-          <img />
-        </div>
-      </Card>
+    <div class="col desc">
+      <p>
+        Mel's new paradigm and the existing ecosystem aren't inherently at odds.
+        The technology letting non-blockchain logic securely access blockchain
+        data also enables cross-chain apps that are completely decentralized and
+        trustless.
+      </p>
+      <p>
+        As an official demo, we are building FancyName, an autonomous relay
+        protocol with EVM-compatible chains. Every Mel-based asset can be
+        accessed on other chains in a trustlessly wrapped form. This also means
+        that wMEL will be the first low-volatility, truly crypto-native asset in
+        the current DeFi ecosystem.
+      </p>
+      <a href="#">Mint your first wMEL</a>
     </div>
   </div>
 </div>
 
-<style lang="scss">
-  @use "../../stylesheets/spacing";
-  span.button.hover {
-    filter: invert(40%);
-  }
-
-  .main-content {
-    margin-top: 5rem;
-    margin-bottom: 15rem;
-  }
-
-  .top {
+<!-- <div class="row talking-point">
+  <div class="col">
+    <div class="card-inner">
+      <h2>Monetary infrastructure for a better future.</h2>
+      <p>
+        The future we are building needs a new kind of money &mdash; $MEL is the
+        the first truly decentralized unit of account, changing everything from
+        payments to incentive design.
+      </p>
+      <img />
+    </div>
+  </div>
+  <div class="col">
+    <div class="card-inner">
+      <h2>From off-chain to cross-chain.</h2>
+      <p>
+        Mel’s new paradigm enabling off-chain composability also makes
+        cross-chain a breeze. And every Mel-based asset can be accessed on
+        EVM-compatible chains through trustless two-way relay contracts.
+      </p>
+      <img />
+    </div>
+  </div>
+</div> -->
+<style>
+  .hero {
+    width: 100%;
     display: flex;
+    align-items: center;
+    justify-content: center;
     flex-direction: column;
-    img {
-      max-width: 100%;
-      border-radius: 1rem;
-      margin: spacing.$default-margin;
-    }
+    padding-top: 40vmin;
+    padding-bottom: 40vmin;
+
+    text-align: center;
+
+    /* background-color: black;
+    color: white; */
+  }
+
+  .invert {
+    background-color: black;
+    color: white;
+  }
+
+  .hero h1 {
+    font-size: 6vmin;
+    font-weight: 600;
+  }
+
+  .hero-text {
+    font-size: 2vmin;
+  }
+
+  .talking-point {
+    margin-bottom: 8rem;
   }
 
   .talking-point p {
-    font-size: 95%;
+    font-size: 110%;
   }
 
   .talking-point h2 {
-    font-weight: 600;
-    letter-spacing: -0.02em;
-    font-size: 150%;
+    font-weight: 500;
+    letter-spacing: -0.03em;
+    font-size: 250%;
   }
 
   .talking-point img {
@@ -116,6 +185,10 @@
 
     border: 1px solid black;
     margin-bottom: 1rem;
+  }
+
+  .talking-point .desc {
+    margin-top: 0.5rem;
   }
 
   .talking-point .card-inner {
