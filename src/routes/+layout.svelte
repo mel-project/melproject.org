140ff41a5fd79e0975663942bd6743995b4f4fb8
nullchinchilla <nullchinchilla@pm.me>
2023-02-11 21:06:32 -0600

    fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,18 +2,7 @@
   import Navbar from "@src/components/page-sections/Navbar.svelte";
 </script>
 
-<svelte:head>
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-    rel="stylesheet"
-  />
-  <link
-    href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
-    rel="stylesheet"
-  />
-</svelte:head>
+<svelte:head />
 
 <Navbar />
 
