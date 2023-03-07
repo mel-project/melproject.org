b0ec54c0ae5849580716f0b718bf29d83327155e
nullchinchilla <nullchinchilla@pm.me>
2023-03-07 09:52:35 -0600

    fix

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,6 @@
 export const twitterUrl = "https://twitter.com/melprojectorg";
 export const discordUrl = "https://discord.gg/qfg35paESn";
-export const telegramUrl = "https://t.me/themeliolabs";
-export const forumUrl = "https://forum.themelio.org";
+export const telegramUrl = "https://t.me/mel_project";
+export const forumUrl = "https://forum.melproject.org";
 export const docsUrl = "https://docs.melproject.org";
 export const blogUrl = "https://medium.com/themelio";
