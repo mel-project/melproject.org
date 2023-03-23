9be7329fe2b514ade4dcac303d57939cfc29d3bc
nullchinchilla <nullchinchilla@pm.me>
2023-03-23 08:34:38 -0500

    fix

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,4 @@
-export const twitterUrl = "https://twitter.com/melprojectorg";
+export const twitterUrl = "https://twitter.com/melproject_org";
 export const discordUrl = "https://discord.gg/qfg35paESn";
 export const telegramUrl = "https://t.me/mel_project";
 export const forumUrl = "https://forum.melproject.org";
