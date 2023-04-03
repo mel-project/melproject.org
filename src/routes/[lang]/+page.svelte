7b512b8b83a9ce07f4b9060b1efbd4096935e642
nullchinchilla <nullchinchilla@pm.me>
2023-04-03 18:01:46 -0400

    neh

--- src/routes/[lang]/+page.svelte
+++ src/routes/[lang]/+page.svelte
@@ -187,7 +187,7 @@
     </div>
   </div> -->
 
-  <div class="divider my-5">Team</div>
+  <!-- <div class="divider my-5">Team</div>
   <div id="team">
     <div class="row">
       <div class="col-lg">
@@ -225,12 +225,6 @@
             />
           </li>
         </ul>
-        <!-- <h4>Community</h4>
-        <ul>
-          <li>
-            <Avatar name="ArtoriaShirou#4266" title="Discord mod" />
-          </li>
-        </ul> -->
       </div>
       <div class="col-lg">
         <h4>Advisors</h4>
@@ -248,7 +242,7 @@
         <img src={PolychainLogo} class="backer" />
       </div>
     </div>
-  </div>
+  </div> -->
 
   <div class="divider my-5">Links</div>
   <div class="links content">
